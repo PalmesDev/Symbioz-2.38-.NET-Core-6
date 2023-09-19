c1a0349666081c08549d63fd2f06f45dd3b19324
Palmes <77801937+PalmesDev@users.noreply.github.com>
2023-09-19 13:24:56 +0200

    Add Auth commands
    Add in Auth "addaccount" command.
    Allows you to create a new account directly in auth console.

--- Symbioz.Auth/CommandsRepertory.cs
+++ Symbioz.Auth/CommandsRepertory.cs
@@ -23,11 +23,38 @@ namespace Symbioz.Auth
         {
             logger.White("Clients Connecteds: " + AuthServer.Instance.ClientsCount);
         }
+
         [ConsoleCommand("banip")]
         public static void BanIp(string input)
         {
             BanIpRecord record = new BanIpRecord(input);
             record.AddInstantElement();
+        }
+
+        [ConsoleCommand("addaccount")]
+        public static void Account(string input)
+        {
+            AccountRecord accountRecord = AccountRecord.GetAccountRecord((int)DatabaseReader<AccountRecord>.Count("Id"));
+            int id = accountRecord.Id;
+            id++;
+
+            Console.Write("username : ");
+            var username = Console.ReadLine();
+
+            Console.Write("password : ");
+            var password = Console.ReadLine();
+
+            Console.WriteLine("nickname : ");
+            var nickname = Console.ReadLine();
+
+            Console.WriteLine("role : ");
+            var roleString = Console.ReadLine();
+            sbyte role = Convert.ToSByte(roleString);
+
+            var newAccount = new AccountRecord(id, username, password, nickname, role, false, 5, 30);
+            DatabaseWriter<AccountRecord>.InstantInsert(newAccount);
+
+            Console.WriteLine("Compte créer avec succès ! ");
         }
     }
 }
